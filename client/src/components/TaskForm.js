2f1d7800c8168e7a6a144e5a3542a3f10bd1cdcb
Gabriel15278 <20233984@aloe.ulima.edu.pe>
2026-02-17 22:25:31 -0500

    Fix: Unificación de client y server en un solo repo

---
+++ client/src/components/TaskForm.js
@@ -0,0 +1,130 @@
+import { Grid, Card, Typography, CardContent, TextField, Button, CircularProgress } from '@mui/material'
+import { useState, useEffect } from 'react'
+import {useNavigate, useParams} from 'react-router-dom'
+
+export default function TaskForm() {
+    const [task, setTask] = useState({
+        title: '',
+        description: ''
+    })
+
+    const [loading, setLoading] = useState(false);
+    const [editing, setEditing] = useState(false);
+
+    const navigate = useNavigate();
+    const params = useParams();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        if(editing){
+            await fetch(`http://localhost:4000/tasks/${params.id}`,{
+                method: 'PUT',
+                body: JSON.stringify(task),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+        }else{
+            await fetch('http://localhost:4000/tasks', {
+                method: 'POST',
+                body: JSON.stringify(task),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        }
+
+        setLoading(false)
+        navigate('/');
+    }
+
+    const handleChange = e => {
+        setTask({ ...task, [e.target.name]: e.target.value });
+    };
+
+    const loadTask = async (id) =>{
+        const res = await fetch(`http://localhost:4000/tasks/${id}`)
+        const data = await res.json();
+        setTask({title: data.title, description: data.description});
+        setEditing(true);
+    }
+
+    useEffect(() => {
+        if(params.id){
+            loadTask(params.id)
+        }
+    },[params.id]);
+
+    return (
+        <Grid
+            container
+            alignItems='center'
+            direction="column"
+            justifyContent="center"
+        >
+            <Grid item xs={3}>
+                <Card
+                    sx={{ mt: 5 }}
+                    style={{
+                        backgroundColor: '#1e272e',
+                        padding: '1rem'
+                    }}
+                >
+                    <Typography variant='5' textAlign='center' color='white'>
+                        {editing ? "Edit Task" : "Create Task"}
+                    </Typography>
+                    <CardContent>
+                        <form onSubmit={handleSubmit}>
+                            <TextField
+                                name='title'
+                                value={task.title}
+                                variant='filled'
+                                label='Write your title'
+                                sx={{
+                                    display: 'block',
+                                    margin: '.5rem 0'
+                                }}
+
+                                slotProps={{
+                                    input: {
+                                        style: { color: 'white' },
+                                    },
+                                    inputLabel: {
+                                        style: { color: 'white' },
+                                    },
+                                }}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                name='description'
+                                value={task.description}
+                                variant='filled'
+                                label='write your description'
+                                multiline
+                                rows={4}
+                                sx={{
+                                    display: 'block',
+                                    margin: '.5rem 0'
+                                }}
+                                slotProps={{
+                                    input: {
+                                        style: { color: 'white' },
+                                    },
+                                    inputLabel: {
+                                        style: { color: 'white' },
+                                    },
+                                }}
+                                onChange={handleChange}
+                            />
+                            <Button variant='contained' color='primary' type='submit' disabled={!task.title || !task.description}>
+                                {loading ? <CircularProgress color='inherit' size={24}/> : 'Save'}
+                            </Button>
+                        </form>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
+    )
+}
